79e4ad3d4197ee739199c9a6e9b91dae1c4c18e8
N1208 <98071560+N1208@users.noreply.github.com>
2026-06-08 18:44:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const button=document.getElementById("recommend");
+
+button.addEventListener("click",()=>{
+
+document.getElementById("result").innerHTML=
+
+`
+<h2>☀️ Sunny</h2>
+
+<p>Positive News</p>
+
+<a href="https://spotify.com">
+
+Open Playlist
+
+</a>
+
+`;
+
+});
